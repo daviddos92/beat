e86cfc0f765ae791966f3507d054df98d9b7f596
daviddos92 <daviddos@gmx.net>
2013-11-22 19:28:15 +0100

    test ihr süßen

--- src/general/Menu.java
+++ src/general/Menu.java
@@ -1,11 +1,14 @@
 package general;
 
 import java.awt.GridLayout;
+import java.awt.PopupMenu;
 
 public class Menu{
 	
 	public Menu(){
 		Window fMenu = new Window("Menü");
 		fMenu.setLayout(new GridLayout(3,1));
+		PopupMenu test = null;
+		fMenu.add(test);
 	}
 }
