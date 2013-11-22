d0754d93e3ab73254246f3e196735827b6998745
daviddos92 <daviddos@gmx.net>
2013-11-22 19:49:01 +0100

    yo

--- src/general/Menu.java
+++ src/general/Menu.java
@@ -1,14 +1,38 @@
 package general;
+import org.lwjgl.*;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.LWJGLException;
+
 
 import java.awt.GridLayout;
 import java.awt.PopupMenu;
 
 public class Menu{
 	
-	public Menu(){
-		Window fMenu = new Window("Menü");
-		fMenu.setLayout(new GridLayout(3,1));
-		PopupMenu test = null;
-		fMenu.add(test);
+	public void start() {
+		try {
+			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		// init OpenGL here
+		
+		while (!Display.isCloseRequested()) {
+			
+			// render OpenGL here
+			
+			Display.update();
+		}
+		
+		Display.destroy();
+	}
+	
+	public static void main(String[] argv) {
+		Menu displayExample = new Menu();
+		displayExample.start();
 	}
 }
