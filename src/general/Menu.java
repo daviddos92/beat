83ab321217f9475446dd5157c7133e053fd296a4
daviddos92 <daviddos@gmx.net>
2013-11-22 23:29:05 +0100

    Button

--- src/general/Menu.java
+++ src/general/Menu.java
@@ -1,8 +1,11 @@
 package general;
 import org.lwjgl.*;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.LWJGLException;
+
+
 
 
 import java.awt.GridLayout;
@@ -10,9 +13,11 @@ import java.awt.PopupMenu;
 
 public class Menu{
 	
+	private static final String TEXPATH = "C:/Users/David Schulte/Desktop/GameJam/bin/general/test2.png";
+	private static final String TEXPATH2 = "C:/Users/David Schulte/Desktop/GameJam/bin/general/test.png";
 	public void start() {
 		try {
-			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.setDisplayMode(new DisplayMode(1280,720));
 			Display.create();
 		} catch (LWJGLException e) {
 			e.printStackTrace();
@@ -20,12 +25,29 @@ public class Menu{
 		}
 		
 		// init OpenGL here
+		Button testbutton=new Button();
+		testbutton.addButton(120,100, TEXPATH);
+		
+		Button testbutton2=new Button();
+		testbutton2.addButton(300,250, TEXPATH2);
+		
+//		
+//		Button testbutton3=new Button();
+//		testbutton3.addButton(450,400, TEXPATH);
 		
 		while (!Display.isCloseRequested()) {
 			
 			// render OpenGL here
-			
+//			int mousex = Mouse.getX(); // will return the X coordinate on the Display.
+//			int mousey = Mouse.getY(); // will return the Y coordinate on the Display.
+		
+			testbutton.Draw();
+			testbutton2.Draw();
+//			testbutton3.Draw();
 			Display.update();
+
+		Display.sync(60);
+			
 		}
 		
 		Display.destroy();
