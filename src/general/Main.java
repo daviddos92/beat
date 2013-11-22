585b969ef63ccc43ec1c4f46af1afe5700e00dfa
daviddos92 <daviddos@gmx.net>
2013-11-22 18:47:05 +0100

    start

---
+++ src/general/Main.java
@@ -0,0 +1,13 @@
+package general;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Window("Test");
+
+	}
+
+}
---
+++ src/general/Menu.java
@@ -0,0 +1,11 @@
+package general;
+
+import java.awt.GridLayout;
+
+public class Menu{
+	
+	public Menu(){
+		Window fMenu = new Window("Menü");
+		fMenu.setLayout(new GridLayout(3,1));
+	}
+}
