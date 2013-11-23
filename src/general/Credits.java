ca3a6fe4bf4841c4ae400053c21ee44f1f7e86af
ubertroll <lennarthoffmann@gmx.de>
2013-11-23 17:54:03 +0100

    option menü dingens

--- src/general/Credits.java
+++ src/general/Credits.java
@@ -4,28 +4,28 @@ import static org.lwjgl.opengl.GL11.*;
 import org.lwjgl.opengl.Display;
 
 public class Credits{
-	public boolean start(){
-		Button black = new Button();
-		black.addButton(0, 0, "../gamejam/src/general/schwarz.png");
-		
-		Button credit = new Button();
-		credit.addButton(200, 200, "../gamejam/src/general/lecredits.png");
-		
-		
-		int x1 = 0;
-		int y = 0;
-		while (!Display.isCloseRequested()) {
-			black.Draw();
-			credit.addButton(100, x1, "../gamejam/src/general/lecredits.png");				
-			credit.Draw();	
-			y++;
-			if (y==10){
-				x1++;
-				y=0;
-			}
-			Display.update();
-		}		
-		
-		return false;				
-	}
+        public boolean start(){
+                Button black = new Button();
+                black.addButton(0, 0, "../gamejam/src/general/schwarz.png");
+                
+                Button credit = new Button();
+                credit.addButton(200, 200, "../gamejam/src/general/lecredits.png");
+                
+                
+                int x1 = 0;
+                int y = 0;
+                while (!Display.isCloseRequested()) {
+                        black.Draw();
+                        credit.addButton(100, x1, "../gamejam/src/general/lecredits.png");                                
+                        credit.Draw();        
+                        y++;
+                        if (y==10){
+                                x1++;
+                                y=0;
+                        }
+                        Display.update();
+                }                
+                
+                return false;                                
+        }
 }
--- src/general/settings.java
+++ src/general/settings.java
@@ -2,6 +2,6 @@ package general;
 
 public class settings {
 
-	public static int deltaMove=1;
+	public static int deltaMove=4;
 	public static int deltaHit=5;
 }
